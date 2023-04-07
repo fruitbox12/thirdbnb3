9ae917d594a9dd90753565f6266771688d8327bb
fruitbox12 <dylanwong007@gmail.com>
2023-04-07 10:45:25 -0700

    Update ConnectUNSD.tsx

--- src/components/ConnectUNSD.tsx
+++ src/components/ConnectUNSD.tsx
@@ -1,51 +1,41 @@
 import { useState } from "react";
-import UAuth from "@uauth/js";
+import { Magic } from "magic-sdk";
 import { Button } from "@chakra-ui/react";
 
-const uauth = new UAuth({
-  clientID: "6f5088be-dc48-4799-9c68-99418f13325c",
-  redirectUri: "https://thirdbnb.vercel.app/",
-  scope: "openid wallet",
-});
+const magic = new Magic("pk_live_47057EC7DC7D2202");
 
-function ConnectUNSD() {
-  const [Uauth, setUauth] = useState();
+function ConnectMagic() {
+  const [userMetadata, setUserMetadata] = useState();
 
-  async function Connect() {
+  async function connect() {
     try {
-      const authorization = await uauth.loginWithPopup();
-      setUauth(JSON.parse(JSON.stringify(authorization))["idToken"]);
-
-      // eslint-disable-next-line no-undef
-      await authenticate();
+      await magic.auth.loginWithMagicLink({ email: "your@email.com" });
+      const metadata = await magic.user.getMetadata();
+      setUserMetadata(metadata);
     } catch (error) {
       console.error(error);
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async function logOut() {
-    uauth.logout();
-    logOut();
+    await magic.user.logout();
+    setUserMetadata(null);
   }
 
   function log() {
-    if (Uauth === null || Uauth === undefined) {
-      Connect();
+    if (userMetadata === null || userMetadata === undefined) {
+      connect();
     } else {
-      // logOut();
+      logOut();
     }
   }
 
-  const getEllipsisTxt = (str: string | any[], n = 4) => {
+  const getEllipsisTxt = (str, n = 4) => {
     if (str) {
       return `${str.slice(0, n)}...${str.slice(str.length - n)}`;
     }
     return "";
   };
-
-  // console.log(Uauth.wallet_address);
-  // console.log(Uauth.sub);
 
   return (
     <>
@@ -61,15 +51,12 @@ function ConnectUNSD() {
         maxW="100%"
         onClick={log}
       >
-        {Uauth != null
-          ? Uauth["sub"] + " as " + getEllipsisTxt(Uauth["wallet_address"])
-          : "Login UNSDomain"}
+        {userMetadata != null
+          ? userMetadata.email + " as " + getEllipsisTxt(userMetadata.publicAddress)
+          : "Login with Magic"}
       </Button>
     </>
   );
 }
 
-export default ConnectUNSD;
-function authenticate() {
-  throw new Error("Function not implemented.");
-}
+export default ConnectMagic;
