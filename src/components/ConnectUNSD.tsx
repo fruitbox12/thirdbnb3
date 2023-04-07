61f0544efe01bdc0009f0bbc85765e850fc4475d
fruitbox12 <dylanwong007@gmail.com>
2023-04-07 11:08:49 -0700

    Update ConnectUNSD.tsx

--- src/components/ConnectUNSD.tsx
+++ src/components/ConnectUNSD.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import { Magic } from "magic-sdk";
-import { Button } from "@chakra-ui/react";
-
-const magic = new Magic("pk_live_47057EC7DC7D2202");
 
 function ConnectMagic() {
-  const [userMetadata, setUserMetadata] = useState();
+  const [userMetadata, setUserMetadata] = useState<Magic.MagicUserMetadata | null>(null);
 
   async function connect() {
     try {
